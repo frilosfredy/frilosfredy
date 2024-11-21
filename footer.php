844d2aaa5af6770c558d5cc19663cf03c6174846
frilosfredy <126310279+frilosfredy@users.noreply.github.com>
2024-11-21 15:14:42 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,28 @@
+
+ <style>
+        body {
+            margin: 0;
+            font-family: Arial, sans-serif;
+        }
+
+       
+
+        footer {
+            position: fixed;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            background-color: #262826;
+            color: white;
+            text-align: center;
+            padding: 0px;
+        }
+    </style> 
+</head>
+<body>
+    
+    <footer>
+        <p>&copy; 2024 Government Service Portal. Always at the bottom!</p>
+    </footer>
+</body>
+</html>
