f39e84dd182f9c702ab86fee06a35da16a0f7926
frilosfredy <126310279+frilosfredy@users.noreply.github.com>
2024-11-21 15:11:40 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+// Database configuration
+define("DB_HOST", "localhost");         // Database host
+define("DB_NAME", "govapp_db");   // Database name
+define("DB_USER", "db_username");       // Database user
+define("DB_PASS", "db_password");       // Database password
+
+// Site URL
+define("BASE_URL", "http://localhost/govapphelper"); // Base URL of the website
+define("APP_PATH", "C:/xampp/htdocs/govapphelper");
