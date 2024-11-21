be0d221fd2568472eb711d72d83ddc9320651485
frilosfredy <126310279+frilosfredy@users.noreply.github.com>
2024-11-21 15:16:20 +0530

    Create create_aadhar.php

---
+++ create_aadhar.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Check Aadhaar Status</title>
+     <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f9f9f9;
+        }
+
+        .main-content {
+            max-width: 1000px;
+            margin: 0px auto;
+            background: #fff;
+            padding: 20px;
+            border: 1px solid #ddd;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+            
+        }
+
+        .content-section ul {
+            padding: 0;
+            list-style: none;
+        }
+
+        .content-section ul li {
+            margin: 10px 0;
+        }
+
+        .content-section ul li strong {
+            color: #28527a;
+        }
+
+        .content-section img {
+            display: block;
+            margin: 20px 0;
+            width: 800px;
+            height: auto;
+        } 
+    </style>
+</head>
+<body>
+    
+        <!-- Content for Aadhaar Section -->
+        <div id="create_aadhar.php" class="content-section">
+            <h1>Check Aadhaar Status</h1>
+            <h2>Online Method via UIDAI Website</h2>
+            <ul>
+                <li><strong>Step 1:</strong> Visit the <a href="https://uidai.gov.in" target="_blank">official UIDAI website</a>.</li>
+                <li><strong>Step 2:</strong> Navigate to the <em>"My Aadhaar"</em> section.</li>
+                <li><strong>Step 3:</strong> Click on <em>"Check Aadhaar Status"</em>.</li>
+                <img src="http://localhost/govapphelper/guides/aadhar/_images/pic.png" alt="Screenshot of the UIDAI website showing Aadhaar status check page"/>
+                <li><strong>Step 4:</strong> Enter your 12-digit Aadhaar Enrollment ID (EID), which is found on the acknowledgment slip provided during enrollment. Include the date and time details printed on it.</li>
+                <li><strong>Step 5:</strong> Enter the security CAPTCHA code.</li>
+                <li><strong>Step 6:</strong> Click "Check Status".</li>
+            </ul>
+        </div>
+    
+</body>
+</html>
